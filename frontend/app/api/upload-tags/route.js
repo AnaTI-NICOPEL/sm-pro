d247e3efc4a16cf882bd14e0d209602a5fbcab0f
ana-clara-souza <souzaanac13@gmail.com>
2026-06-30 11:28:23 -0300

    Nova aba para atualizar etiquetas

--- frontend/app/api/upload-tags/route.js
+++ frontend/app/api/upload-tags/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
-import { pgPool } from '../../../../lib/db';
+import { pgPool } from '../../../lib/db';
 
 export const dynamic = 'force-dynamic';
 
